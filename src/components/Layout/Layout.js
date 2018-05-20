1c3d6111442b8671a815d9ea820d4a5dc81c7ffc
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2018-05-20 14:03:33 +0545

    SideDrawer updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Auxi from '../../hoc/Auxi';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 
-const layout = (props) => (
-    <Auxi>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxi>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    render() {
+        return (
+            <Auxi>
+                <Toolbar/>
+                <SideDrawer
+                    open = {this.state.showSideDrawer}
+                    closed = {this.sideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxi>
+        )
+    }
+}
+
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,9 +7,11 @@ const toolbar = () => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
-            <Logo/>
-            <nav>
-                <NavigationItems/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
+            <nav className={classes.DesktopOnly}>
+                <NavigationItems />
             </nav>
         </header>
     )
