5ac40231a278a30ac4fdc8a04358e8ef87de9af9
bhuwanadhikari <adhikaribhuwan97@gmail.com>
2018-06-05 22:31:36 +0545

    folder restructured

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 
@@ -139,4 +140,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Auxi from '../Auxi/Auxi';
+
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+      state = {
+          error: null
+      };
+      componentDidMount(){
+          axios.interceptors.request.use(req => {
+              this.setState({error: null});  // to clear the error
+              return req;
+          });
+          axios.interceptors.response.use (res => res , error => {
+              this.setState({error: error}); // setting error in case of error
+          });
+      }
+
+      //on clicking on backdrop displayed error will be thrown away
+      errorConfirmedHandler = () => {
+          this.setState({error: null})
+      };
+
+      render(){
+          return (
+              <Auxi>
+                  <Modal
+                      show = {this.state.error}
+                      clicked = {this.state.errorConfirmedHandler}
+                  >
+                      {this.state.error ? this.state.error.message : null}
+                  </Modal>
+                  <WrappedComponent{...this.props}/>
+              </Auxi>
+          );
+      }
+  };
+};
+
+export default withErrorHandler;
